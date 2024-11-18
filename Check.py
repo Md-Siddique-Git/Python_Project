a8dc749d3eb562d162f53eafce6b5fb3d9bb9320
MD SIDDIQUE <76256885+Md-Siddique-Git@users.noreply.github.com>
2024-11-18 08:54:05 +0000

    updated

--- Check.py
+++ Check.py
@@ -5,10 +5,13 @@ y=int(input("Enter your 2nd no: "));
 sum=x+y;
 sub=y-x;
 mul=x*y;
-Operation=input("enter your operation to perform : (sum,sub,mul) ")
+divide=y/x;
+Operation=input("enter your operation to perform : (sum,sub,mul,divide) ")
 if Operation=='sub':
     print("your subtraction value is ",sub)
 if Operation=='sum':
     print("your sum is ",sum)
 if Operation=='mul':
     print("your product is ",mul)
+if Operation=='divide':
+    print("your output is ",divide)
